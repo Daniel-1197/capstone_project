8db1bf0312951d4e3929654f50d53909b3f6cefd
Daniel Prem <danielprem1197@gmail.com>
2025-04-29 09:06:55 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,12 +1,12 @@
 import pytest
 from selenium import webdriver
-# from selenium.webdriver.chrome.service import Service
-# from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture(scope="function")
 def driver():
     """Initialize the webdriver and return driver instance"""
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     driver.maximize_window()
     yield driver
     driver.quit()
